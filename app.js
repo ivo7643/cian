b547c1e68574e302360191fd76c7401bf85521fa
Maks Zhovna <maks.zhovna@gmail.com>
2023-07-19 18:28:25 +0300

    server

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-console.log("Hello World!");
+require('@babel/register');
+const express = require('express');
+const serverConfig = require('./config/serverConfig');
+const app = express();
 
-// start
+const PORT = 3010;
+serverConfig(app);
+
+// const indexRoute = require('./routes/index.route');
+
+// app.use('/', indexRoute);
+
+app.listen(PORT, async () => {
+  console.log(`Server started on port ${PORT}`);
+  // try {
+  //   await sequelize.authenticate();
+  //   console.log("Connection BD has been established successfully.");
+  // } catch (error) {
+  //   console.error("Unable to connect to the database:", error);
+  // }
+});
