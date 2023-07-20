8374d9edb4392dab3027b334fd74bfe4db8aed39
JerseyPl <Delr1602@gmail.com>
2023-07-20 19:39:57 +0300

    favoriteview

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 const mainRouter = require("./view/main.route");
 const authRouter = require("./view/auth.route");
 const apiAuthRouter = require("./api/apiAuth.route");
-const adminApartmentsRouter = require('./view/adminApartments.route');
-const ApartmentsRouter = require('./api/apiAdminApartments.route');
+const adminApartmentsRouter = require("./view/adminApartments.route");
+const ApartmentsRouter = require("./api/apiAdminApartments.route");
+const Favorites=require('./view/favorites.route')
 
-router.use('/adminApartments', adminApartmentsRouter);
+router.use("/favorites",Favorites);
+router.use("/adminApartments", adminApartmentsRouter);
 router.use("/", mainRouter);
 router.use("/auth", authRouter);
 router.use("/api/auth", apiAuthRouter);
 
-router.use('/api/apartments', ApartmentsRouter)
+router.use("/api/apartments", ApartmentsRouter);
 
 module.exports = router;
