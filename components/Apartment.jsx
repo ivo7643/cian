c25d77cfc1d0075c2790506748ad9ae4d935e91b
ivo7643 <nahdora001@gmail.com>
2023-07-20 16:58:01 +0300

    Filter and Router

---
+++ components/Apartment.jsx
@@ -0,0 +1,23 @@
+const React = require("react");
+
+module.exports = function Apartment({ apartment }) {
+  return (
+    <div className="col s4 apartmentOne " data-id={apartment.id}>
+      <div className="card apartment deep-purple darken-3">
+        <div className="card-image">
+          <img src={apartment.Photos[0].url} />
+          <span className="card-title">{apartment.name}</span>
+        </div>
+        <div className="card-content">
+          <span className="card-title">Стоимость: {apartment.price}</span>
+        </div>
+        <div className="card-content">
+          <span className="card-title">Описание: {apartment.description}</span>
+        </div>
+        <div className="card-action">
+          <a href={`/apartments/${apartment.id}`}>Узнать подробнее</a>
+        </div>
+      </div>
+    </div>
+  );
+};
