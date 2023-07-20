63683f5f7bea6a2a711392958abb7a8ca792e946
ivo7643 <nahdora001@gmail.com>
2023-07-20 19:30:57 +0300

    Visit apartment

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,13 +2,15 @@ const router = require("express").Router();
 const mainRouter = require("./view/main.route");
 const authRouter = require("./view/auth.route");
 const apiAuthRouter = require("./api/apiAuth.route");
-const adminApartmentsRouter = require('./view/adminApartments.route');
-const ApartmentsRouter = require('./api/apiAdminApartments.route');
+const adminApartmentsRouter = require("./view/adminApartments.route");
+const ApartmentsRouter = require("./api/apiAdminApartments.route");
+const ApartmentCard = require("../routes/view/apartment.route");
 
-router.use("/", mainRouter);
 router.use("/auth", authRouter);
 router.use("/api/auth", apiAuthRouter);
-router.use('/adminApartments', adminApartmentsRouter);
-router.use('/api/apartments', ApartmentsRouter)
+router.use("/adminApartments", adminApartmentsRouter);
+router.use("/api/apartments", ApartmentsRouter);
+router.use("/apartments", ApartmentCard);
+router.use("/", mainRouter);
 
 module.exports = router;
