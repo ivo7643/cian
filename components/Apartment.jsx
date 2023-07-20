3d3b61477ba06bc06b7939606faf61ad6ed939e7
ivo7643 <nahdora001@gmail.com>
2023-07-20 17:17:47 +0300

    Fix FIlter

--- components/Apartment.jsx
+++ components/Apartment.jsx
@@ -5,7 +5,7 @@ module.exports = function Apartment({ apartment }) {
     <div className="col s4 apartmentOne " data-id={apartment.id}>
       <div className="card apartment deep-purple darken-3">
         <div className="card-image">
-          <img src={apartment.Photos[0].url} />
+          {apartment.Photos.length > 0 && <img src={apartment.Photos[0].url} />}
           <span className="card-title">{apartment.name}</span>
         </div>
         <div className="card-content">
